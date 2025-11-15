ce98c167230e42e4cc865ddeab2b6ab1bd40e618
Gustavo Vinícius <88352305+gostavovinicius@users.noreply.github.com>
2025-11-15 14:30:07 -0300

    efeitos de natal

---
+++ natal.js
@@ -0,0 +1,7 @@
+// Conteúdo do natal.js
+snowStorm.autoStart = true;
+snowStorm.flakesMaxActive = 60;
+snowStorm.useTwinkleEffect = true;
+snowStorm.vMaxX = 3;
+snowStorm.vMaxY = 3;
+snowStorm.followMouse = true;
