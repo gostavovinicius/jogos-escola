5ce1126ba6b508a001c8eb0b3e0e90bd10c033b4
Gustavo Vinícius <88352305+gostavovinicius@users.noreply.github.com>
2025-12-05 14:05:05 -0300

    Update theme

--- natal.js
+++
@@ -1,7 +0,0 @@
-// Conteúdo do natal.js
-snowStorm.autoStart = true;
-snowStorm.flakesMaxActive = 60;
-snowStorm.useTwinkleEffect = true;
-snowStorm.vMaxX = 3;
-snowStorm.vMaxY = 3;
-snowStorm.followMouse = true;
